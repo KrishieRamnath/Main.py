fee34fad0d3be9aea2b2ade248059488a029af83
KrishieRamnath <139908176+KrishieRamnath@users.noreply.github.com>
2023-07-24 10:39:53 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-#churn_data = pd.read_excel("/Users/krishier/Library/CloudStorage/OneDrive-Personal/Columbia Summer School/Final Project 5/DataSet.xlsx")
+churn_data = pd.read_excel("/Users/krishier/Library/CloudStorage/OneDrive-Personal/Columbia Summer School/Final Project 5/DataSet.xlsx")
 print(churn_data.head())
